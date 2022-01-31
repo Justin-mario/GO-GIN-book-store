746410812efb5b261795668353381b73cbdefb64
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2022-02-01 00:00:33 +0100

    bookstore

--- controller/handler.go
+++ controller/handler.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"github.com/Justin-mario/GO-GIN-book-store/model"
+	"github.com/gin-gonic/gin"
+)
+
+type BookController interface {
+	FindAll() []model.Bookstore
+	Save(ctx gin.Context)
+}
